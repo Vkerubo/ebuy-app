0d364da745fd2c18b341824e7af06e15d54c12d7
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-02 23:44:58 +0300

    fetch data

--- ebuy-app/src/Context/ProductContext.js
+++ ebuy-app/src/Context/ProductContext.js
@@ -6,7 +6,15 @@ export const ProductContext = createContext();
 const productprovider = ({ children }) => {
     // products state
     const [products, setProducts] = useState([]);
-    
+    // fetch products
+    useEffect(() => {
+        const fetchProducts = async () =>{
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+            setProducts(data);
+        };
+    }
+
 }
      
 
