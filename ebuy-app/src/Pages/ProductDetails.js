b231d0e4569b69bf41e257b16d290fefe359d062
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-03 12:18:56 +0300

    add react-router-dom

--- ebuy-app/src/Pages/ProductDetails.js
+++ ebuy-app/src/Pages/ProductDetails.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react';
+// import useParams
+import { useParams } from 'react-router-dom';
+// import cart context
+import { CartContext } from '../contexts/CartContext';
+// import product context
+import { ProductContext } from '../contexts/ProductContext';
 
-export const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
-}
+const ProductDetails = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
 export default ProductDetails;
