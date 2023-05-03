33e2523a93c54db2ed6997c0f7fa30031b40263d
sandra <nyamweasandra@gmail.com>
2023-05-03 12:47:00 +0300

    returning links

--- ebuy-app/src/Components/CartItem.js
+++ ebuy-app/src/Components/CartItem.js
@@ -15,21 +15,21 @@ const CartItem = ({ item }) => {
     return (
         <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
           <div className='w-full min-h-[150px] flex items-center gap-x-4'>
-            {/* image */}
+            /* image */
             <Link to={`/product/${id}`}>
               <img className='max-w-[80px]' src={image} alt='' />
             </Link>
             <div className='w-full flex flex-col'>
-              {/* title & remove icon */}
+              /* title & remove icon */
               <div className='flex justify-between mb-2'>
-                {/* title */}
+                /* title */
                 <Link
                   to={`/product/${id}`}
                   className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'
                 >
                   {title}
                 </Link>
-                {/* remove icon */}
+                /* remove icon */
                 <div
                   onClick={() => removeFromCart(id)}
                   className='text-xl cursor-pointer'
@@ -38,20 +38,20 @@ const CartItem = ({ item }) => {
                 </div>
               </div>
               <div className='flex gap-x-2 h-[36px] text-sm'>
-                {/* qty */}
+                /* qty */
                 <div className='flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
-                  {/* minus icon */}
+                  /* minus icon */
                   <div
                     onClick={() => decreaseAmount(id)}
                     className='flex-1 flex justify-center items-center cursor-pointer h-full'
                   >
                     <IoMdRemove />
                   </div>
-                  {/* amount */}
+                  /* amount */
                   <div className='h-full flex justify-center items-center px-2'>
                     {amount}
                   </div>
-                  {/* plus icon */}
+                  /* plus icon */
                   <div
                     onClick={() => increaseAmount(id)}
                     className='flex-1 h-full flex justify-center items-center cursor-pointer'
@@ -59,13 +59,21 @@ const CartItem = ({ item }) => {
                     <IoMdAdd />
                   </div>
                 </div>
-                {/* item price */}
+                /* item price */
                 <div className='flex-1 flex items-center justify-around'>
                   $ {price}
                 </div>
-                {/* final price */}
-                {/* make the price at 2 decimals */}
+                /* final price */
+                /* make the price at 2 decimals */
                 <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(
                   price * amount
-                )
+                ).toFixed(2)}`}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      };
+      
+      export default CartItem;
 
