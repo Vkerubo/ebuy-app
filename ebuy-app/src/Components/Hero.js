e5ac4ea24a77ce0cc207b759d6ac6a87d9b189a0
Valentine <kerubovalentine1@gmail.com>
2023-05-03 15:59:37 +0300

    add container className

--- ebuy-app/src/Components/Hero.js
+++ ebuy-app/src/Components/Hero.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24"></section>
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justify-around h-full"></div>
+    </section>
   );
 };
 export default Hero;
