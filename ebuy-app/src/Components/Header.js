82f111807d571627f2bc078adfb5774fbfd8fb0c
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-03 15:59:24 +0300

    fixed header,js

--- ebuy-app/src/Components/Header.js
+++ ebuy-app/src/Components/Header.js
@@ -22,30 +22,30 @@ useEffect(() => {
   });
 });
 return (
-    <header
-      className={`${
-        isActive ? 'bg-white py-4 shadow-md' : 'bg-none py-6'
-      } fixed w-full z-10 transition-all`}
-    >
-      <div className='container mx-auto flex items-center justify-between h-full'>
-        {/* logo */}
-        <Link to={'/'}>
-          <div>
+<header
+    className={`${
+       isActive ? 'bg-white py-4 shadow-md' : 'bg-none py-6'
+         } fixed w-full z-10 transition-all`}
+            >
+              <div className='container mx-auto flex items-center justify-between h-full'>
+                 {/* logo */}
+<Link to={'/'}>
+        <div>
             <img className='w-[40px]' src={Logo} alt='' />
-          </div>
-        </Link>
-        {/* cart */}
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className='cursor-pointer flex relative'
-        >
-          <BsBag className='text-2xl' />
-          <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>
-            {itemAmount}
-          </div>
         </div>
-      </div>
-    </header>
+</Link>
+    {/* cart */}
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+           className='cursor-pointer flex relative'
+                >
+             <BsBag className='text-2xl' />
+          <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>
+        {itemAmount}
+</div>
+        </div>
+</div>
+</header>
   );
 };
 
