6326661f7112abdc794f6697876ad8cad5438fde
sandra <nyamweasandra@gmail.com>
2023-05-03 15:56:15 +0300

    more commits

--- ebuy-app/src/index.js
+++ ebuy-app/src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// product provider
+import ProductProvider from './contexts/ProductContext';
+// sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
+// cart provider
+import CartProvider from './contexts/CartContext';
 
 
