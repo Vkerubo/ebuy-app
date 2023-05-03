f453de7c83120881b7044edc564dc5b6547c2249
Valentine <kerubovalentine1@gmail.com>
2023-05-03 16:02:10 +0300

    add pretitle

--- ebuy-app/src/Components/Hero.js
+++ ebuy-app/src/Components/Hero.js
@@ -5,7 +5,13 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
-      <div className="container mx-auto flex justify-around h-full"></div>
+      <div className="container mx-auto flex justify-around h-full">
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] bg-red-500 mr-3"></div>New Trend
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
