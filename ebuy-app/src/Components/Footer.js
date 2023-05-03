996ce54636d938d6abf94e17721b3027e5fdca86
Valentine <kerubovalentine1@gmail.com>
2023-05-03 11:43:59 +0300

    export Footer

--- ebuy-app/src/Components/Footer.js
+++ ebuy-app/src/Components/Footer.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const Footer = () => {
-  return <footer className="bg-primary py-12"></footer>;
+const Footer = () => {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; eBuy 2023. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
+export default Footer;
