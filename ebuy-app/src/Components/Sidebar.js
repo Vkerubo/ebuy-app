32ff38634330e3c11b1944857611c4e3ee38ea78
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-03 12:11:06 +0300

    Ternary operator

--- ebuy-app/src/Components/Sidebar.js
+++ ebuy-app/src/Components/Sidebar.js
@@ -17,9 +17,17 @@ const Sidebar = () => {
     const { cart, clearCart, total, itemAmount } = useContext(CartContext);
 
     return (
-        <div>
-            
-        </div>
+        <div
+        className={`${
+          isOpen ? 'right-0' : '-right-full'
+        }  w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+      >
+        <div className='flex items-center justify-between py-6 border-b'>
+          <div className='uppercase text-sm font-semibold'>
+            Shopping Bag ({itemAmount})
+          </div>
+          {/* icon */}
+  
     )
 
 };
