a5bfd58b5ea3d662c7ade0433445d55a907240fb
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-03 15:39:11 +0300

    Complete App.js

--- ebuy-app/src/App.js
+++ ebuy-app/src/App.js
@@ -12,12 +12,18 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-       <Header />
-       <Sidebar />
-       <Footer />
+    <div className='overflow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
     </div>
-  )
-}
+  );
+};
 
 export default App;
