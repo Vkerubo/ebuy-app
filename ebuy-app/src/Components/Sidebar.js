d735118d849e915599f4da0d6145d49d732ce515
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-03 12:17:00 +0300

    styling

--- ebuy-app/src/Components/Sidebar.js
+++ ebuy-app/src/Components/Sidebar.js
@@ -34,6 +34,11 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
+        {cart.map((item) => {
+          return <CartItem item={item} key={item.id} />;
+        })}
+      </div>
   
     )
 
