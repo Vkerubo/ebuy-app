dc29ebd895a9b5e71139555f44336d73d8298938
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-03 15:38:21 +0300

    import links

--- ebuy-app/src/App.js
+++ ebuy-app/src/App.js
@@ -1,8 +1,23 @@
-import "./App.css";
-import React from "react"
+import React from 'react';
+// import react router dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+// import components
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
-  return <div>App</div>;
+
+const App = () => {
+  return (
+    <div>
+       <Header />
+       <Sidebar />
+       <Footer />
+    </div>
+  )
 }
 
 export default App;
