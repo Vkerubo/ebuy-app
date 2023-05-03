16f5da5305ff1658b2a30dc622f9c6abd2c09252
Valentine <kerubovalentine1@gmail.com>
2023-05-03 15:58:21 +0300

    Hero section className

--- ebuy-app/src/Components/Hero.js
+++ ebuy-app/src/Components/Hero.js
@@ -3,6 +3,8 @@ import WomanImg from "../img/woman_hero.png";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24"></section>
+  );
 };
 export default Hero;
