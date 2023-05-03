22a6747685ca0eae2e68b89e3cec7084df350061
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-03 15:42:46 +0300

    import react-router

--- ebuy-app/src/Components/Header.js
+++ ebuy-app/src/Components/Header.js
@@ -1,7 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 // sidebar context
 import { SidebarContext } from '../contexts/SidebarContext';
-
+// cart context
+import { CartContext } from '../contexts/CartContext';
+// import icons
+import { BsBag } from 'react-icons/bs';
+// import link
+import { Link } from 'react-router-dom';
 
 
 
