e4b662bfc17fcba3ee108460054506b3e5d920bc
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-03 11:39:23 +0300

    add div one

--- ebuy-app/src/Components/Product.js
+++ ebuy-app/src/Components/Product.js
@@ -6,15 +6,22 @@ import {BsPlus, BsEyeFill} from 'react-icons.bs';
     // import cart context
 import {Cartcontext } from '../contexts/Cartcontext';
 
-const product = ({ product }) => {
+const Product = ({ product }) => {
     const { addToCart } = useContext(CartContext);
-       //destructure product
-const { id, image, category, title, price } = product;
-}
-    
-    export const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
-export default Product;
+    // destructure product
+    const { id, image, category, title, price } = product;
+    return (
+      <div>
+        <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
+          <div className='w-full h-full flex justify-center items-center'>
+            {/* image */}
+            <div className='w-[200px] mx-auto flex justify-center items-center'>
+              <img
+                className='max-h-[160px] group-hover:scale-110 transition duration-300'
+                src={image}
+                alt=''
+              />
+            </div>
+          </div>
+          {/* buttons */}
+          
