9f849fef08db68dd9bb9d093b882301a9dc88e0c
Valentine <kerubovalentine1@gmail.com>
2023-05-03 16:12:12 +0300

    add woman img

--- ebuy-app/src/Components/Hero.js
+++ ebuy-app/src/Components/Hero.js
@@ -10,6 +10,19 @@ const Hero = () => {
           <div className="font-semibold flex items-center uppercase">
             <div className="w-10 h-[2px] bg-red-500 mr-3"></div>New Trend
           </div>
+          <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+            STYLISH AUTUMN SALE... <br />
+            <span className="font-semibold">Hurry while stock lasts!</span>
+          </h1>
+          <Link
+            to={"/"}
+            className="self-start uppercase font-semibold border-b-2 border-primary"
+          >
+            Find your style
+          </Link>
+        </div>
+        <div className="hidden lg:block">
+          <img src={WomanImg} alt="" />
         </div>
       </div>
     </section>
