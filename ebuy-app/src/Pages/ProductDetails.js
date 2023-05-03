922ceae94f439035d6c0c1c87983e5d35eeb70c4
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-03 12:42:09 +0300

    add props

--- ebuy-app/src/Pages/ProductDetails.js
+++ ebuy-app/src/Pages/ProductDetails.js
@@ -1,31 +1,32 @@
 import React, { useContext } from 'react';
-// import useParams
+       // import useParams
 import { useParams } from 'react-router-dom';
-// import cart context
+       // import cart context
 import { CartContext } from '../contexts/CartContext';
-// import product context
+         // import product context
 import { ProductContext } from '../contexts/ProductContext';
 
 const ProductDetails = () => {
-    // get the product id from url
-    const { id } = useParams();
-  const { products } = useContext(ProductContext);
-  const { addToCart } = useContext(CartContext);
-     // get the single product based on the id
-     const product = products.find((item) => {
-        return item.id === parseInt(id);
+            // get the product id from url
+const { id } = useParams();
+const { products } = useContext(ProductContext);
+const { addToCart } = useContext(CartContext);
+            // get the single product based on the id
+const product = products.find((item) => {
+    return item.id === parseInt(id);
       });
 
-  // if product is not found
-  if (!product) {
-    return (
-      <section className='h-screen flex justify-center items-center'>
-        Loading...
-      </section>
+            // if product is not found
+if (!product) {
+         return (
+    <section className='h-screen flex justify-center items-center'>
+         Loading...
+    </section>
     );
-  }
+}
+             // destructure product
 
-
+const { title, price, description, image } = product;
 
 
 
