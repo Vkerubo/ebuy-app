416124e71e9ae7c3db5e02d95b859c40d8d72a7d
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-03 12:05:20 +0300

    Import links

--- ebuy-app/src/Components/Sidebar.js
+++ ebuy-app/src/Components/Sidebar.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import React, { useContext } from 'react';
+// import link
+import { Link } from 'react-router-dom';
+// import icons
+import { IoMdArrowForward } from 'react-icons/io';
+import { FiTrash2 } from 'react-icons/fi';
+// import components
+import CartItem from '../components/CartItem';
+// import sidebar context
+import { SidebarContext } from '../contexts/SidebarContext';
+// import cart context
+import { CartContext } from '../contexts/CartContext';
 
-export const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const Sidebar = () => {
+    const { isOpen, handleClose } = useContext(SidebarContext);
+    const { cart, clearCart, total, itemAmount } = useContext(CartContext);
+
+    return (
+        <div>
+            
+        </div>
+    )
+
+};
