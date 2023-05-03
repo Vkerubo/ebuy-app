fd88ef6dc91ac50d546d6f39eeab4fd81e6b71fc
sandra <nyamweasandra@gmail.com>
2023-05-03 12:51:24 +0300

    onClick

--- ebuy-app/src/Components/CartItem.js
+++ ebuy-app/src/Components/CartItem.js
@@ -50,12 +50,7 @@ const CartItem = ({ item }) => {
                   /* amount */
                   <div className='h-full flex justify-center items-center px-2'>
                     {amount}
-                  </div>
-                  /* plus icon */
-                  <div
-                    onClick={() => increaseAmount(id)}
-                    className='flex-1 h-full flex justify-center items-center cursor-pointer'
-                  >
+                  
                     <IoMdAdd />
                   </div>
                 </div>
